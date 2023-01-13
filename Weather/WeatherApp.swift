b63c9682e2b89f6b47eae6468318a1385675e778
SandyTwix <iamsandytwix@gmail.com>
2023-01-14 01:23:50 +0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Руслан Трищенков on 14.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
